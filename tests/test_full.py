73b53bd156b054f7da22343a6398d869278c7dcf
Paul Zhou <paul_zhou@brown.edu>
2021-02-24 20:55:59 -0500

    add unit testing for change_q.py

--- tests/test_full.py
+++ tests/test_full.py
@@ -2,6 +2,7 @@ from actgen.wrappers.torch_interface import test_torch_interface
 from actgen.wrappers.duplicate_actions import test_duplicate_action_env
 from actgen.train import main as train
 from actgen.evaluate_dqn import main as evaluate_dqn
+from actgen.change_q import main as change_q
 
 
 def test_train():
@@ -10,3 +11,7 @@ def test_train():
 
 def test_evaluate_dqn():
     evaluate_dqn(test=True)
+
+
+def test_change_q():
+    change_q(test=True)
